9b3ef33650eb0af21e46d9d39d4cc401a014ba03
Naoya Yamashita <conao3@gmail.com>
2023-04-23 20:58:59 +0900

    fix sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import {Button, defaultTheme, Provider} from '@adobe/react-spectrum';
+import {Button, defaultTheme, Flex, Heading, Provider, View} from '@adobe/react-spectrum';
 
 export function App() {
   return (
-    <Provider theme={defaultTheme} colorScheme='dark' height="100%">
-      <Button
-        variant="accent"
-        onPress={() => alert('Hey there!')}
-      >
-        Hello React Spectrum!
-      </Button>
+    <Provider theme={defaultTheme} colorScheme='dark' minHeight="100vh">
+      <View>
+        <Flex direction="column" maxWidth="1200px" gap="size-300" margin="0 auto">
+          <View>
+            <Heading level={1}>Hello React Spectrum!</Heading>
+            <Button
+              variant="accent"
+              onPress={() => alert('Hey there!')}
+            >
+              Hello React Spectrum!
+            </Button>
+          </View>
+        </Flex>
+      </View>
     </Provider>
   );
 }
